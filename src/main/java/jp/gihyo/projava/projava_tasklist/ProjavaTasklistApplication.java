efe6e2f0f498d4bafd009fc0ee9da42024cace18
Naojirou Hisada <naojirouhisada@gmail.com>
2022-06-18 18:01:06 +0900

    first commit

---
+++ src/main/java/jp/gihyo/projava/projava_tasklist/ProjavaTasklistApplication.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projava.projava_tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjavaTasklistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjavaTasklistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/gihyo/projava/projava_tasklist/ProjavaTasklistApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projava.projava_tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjavaTasklistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
